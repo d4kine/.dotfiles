65bf4d2d4296e272de80f9dd777a36c10cfb2349
Chris Kaubisch <christiankaubisch@gmail.com>
2019-03-03 10:07:31 +0100

    transparency changes and random logon wallpaper and color scheme

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #/home/chris/Downloads/wallpapers/yorha-a2-nier-automata-y108.jpg
-wal -i '/usr/share/pixmaps/DSC01360_Edited.jpg' &
+
+~/bin/start_prismatik.sh &
 
 echo -e "connect 4C:EF:C0:CD:06:A1" | bluetoothctl &
 
